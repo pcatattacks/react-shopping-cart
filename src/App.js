9e39c35dfd4e5d7a298e0c45f197727d4c7e60d7
Pranav Dhingra <pranav.dhingra@gmail.com>
2019-01-16 15:49:39 -0600

    #2 and #3 closed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    import("./static/data/products.json") // mocking api call
+    .then((json) => {
+      this.setState({products: json.products})
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.products.map(pjson => <Product info={pjson} />)}
       </div>
     );
   }
+}
+
+const Product = (props) => {
+  return (
+    <div>
+      <img src={require(`./static/products/${props.info.sku}_1.jpg`)} alt="" />
+      <div>{props.info.title}</div>
+      <div>{props.info.price}</div>
+      <div>{`or ${props.info.installments} x ${(props.info.price / props.info.installments).toFixed(2)}`}</div>
+      <button>Add to cart</button>
+    </div>
+  )
 }
 
 export default App;
